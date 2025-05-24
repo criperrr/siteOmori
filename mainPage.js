3ad70a149a03aec5a097df87a3424f763dd93653
criperrr <gabriel.camaforte@unesp.br>
2025-05-23 22:17:16 -0300

    whitespace && mainPage: improve everything and added cool blur effect

--- mainPage.js
+++ mainPage.js
@@ -1,5 +1,20 @@
+let originalFilterStyle = '';
+
 document.addEventListener('DOMContentLoaded', () => {
     let theme = localStorage.getItem('theme') || 'light';
-    console.log('Current theme:', theme);
     document.body.setAttribute('data-theme', theme);
+    const isDark = (theme === 'dark');
+    originalFilterStyle = isDark ? 'invert()' : '';
+    document.getElementById('back').style.filter = originalFilterStyle;
+});
+
+document.getElementById('header').addEventListener('mouseover', () => {
+    document.getElementById('back').style.filter =
+        originalFilterStyle ? originalFilterStyle + ' blur(3px)' : 'blur(3px)';
+});
+document.getElementById('header').addEventListener('mouseout', () => {
+    let theme = localStorage.getItem('theme') || 'light';
+    const isDark = (theme === 'dark');
+    originalFilterStyle = isDark ? 'invert()' : '';
+    document.getElementById('back').style.filter = originalFilterStyle;
 });
